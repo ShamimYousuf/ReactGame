7579cf3bc0a2a343cca9401e52226e796b0d20ab
shamim.yousuf <shamim.yousuf@gamesys.co.uk>
2020-07-18 19:51:17 +0100

    add async call

--- components/Fun.js
+++ components/Fun.js
@@ -1,22 +1,40 @@
 import React from 'react';
 
 class Fun extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: '',
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: '',
+        };
+    }
 
-  componentDidMount() {
-    this.setState({
-      data: 'Call me',
-    });
-  }
+    componentDidMount() {
+        const result = fetch('https://official-joke-api.appspot.com/jokes/programming/random')
+            .then((response) => response.json())
+            .then((response) => ({
+                q: response[0].setup,
+                a: response[0].punchline,
+            }))
+            .then((data) => {
+                console.log(data.q);
+                console.log(data.a);
+                this.setState({
+                    data,
+                });
+            });
+    }
 
-  render() {
-    return (<h2>{this.state.data}</h2>);
-  }
+    render() {
+        return (
+            <div>
+                <h2>Programming Jokes</h2>
+                <details>
+                    <summary>{this.state.data.q}</summary>
+                    {this.state.data.a}
+                </details>
+            </div>
+        );
+    }
 }
 
 export default Fun;
