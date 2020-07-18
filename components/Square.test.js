7fd1df0250c84bebbae58cc4a00d5aa90502e122
shamim.yousuf <shamim.yousuf@gamesys.co.uk>
2020-07-19 00:01:56 +0100

    add jest

---
+++ components/Square.test.js
@@ -0,0 +1,11 @@
+import Square from './Square';
+
+describe('Given Square component', () => {
+  it('should render a button element', () => {
+
+  });
+
+  it('should call a function on button click', () => {
+
+  });
+});
